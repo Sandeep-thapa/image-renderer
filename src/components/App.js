6dfd08786b38ce7dcf5ff85903ec845bb6bced09
Sandeep Thapa <sandeep.thapa@outlook.com>
2019-07-17 14:33:04 +0300

    Addtion of get request to api and async handling of event

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import axios from 'axios'
 import SearchBar from './SearchBar';
 
-class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
 
-        });
+const UNSPLASH_ACCESS_KEY = process.env.REACT_APP_ACCESS_KEY;
+class App extends React.Component {
+
+    async onSearchSubmit(term) {
+        const response = await axios
+            .get('https://api.unsplash.com/search/photos', {
+                params: { query: term },
+                headers: {
+                    Authorization: 'Client-ID ' + UNSPLASH_ACCESS_KEY
+                }
+            });
+        console.log(response.data.results);
     }
+
     render() {
         return (
             <div className="ui container" style={{ marginTop: '50px' }}>
