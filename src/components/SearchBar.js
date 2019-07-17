a3d626bb4d8c33e93253eb2d12c3ed5830783055
Sandeep Thapa <sandeep.thapa@outlook.com>
2019-07-17 09:42:33 +0300

    User input and event handling tuto start

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import App from './App';
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <input type="text" />
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
