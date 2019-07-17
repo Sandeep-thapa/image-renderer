ba5eab39de13d7de973170ae3e92649602f0d6b2
Sandeep Thapa <sandeep.thapa@outlook.com>
2019-07-17 11:11:10 +0300

    Addition of axios for api requests

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import axios from 'axios'
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+class App extends React.Component {
+    onSearchSubmit(term) {
+        axios.get('https://api.unsplash.com/search/photos', {
+
+        });
+    }
+    render() {
+        return (
+            <div className="ui container" style={{ marginTop: '50px' }}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    }
 }
 
 export default App;
