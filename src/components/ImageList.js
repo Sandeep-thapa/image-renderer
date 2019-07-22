2d6fa1e81b4644c829abda9f6aef296312f245c9
Sandeep Thapa <sandeep.thapa@outlook.com>
2019-07-22 10:16:11 +0300

    Added event handlers and updated grid

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './ImageList.css';
+import './ImageCard';
+import ImageCard from './ImageCard';
 
 const ImageList = props => {
     const images = props.images.map((image) => {
         return (
-            <img alt={image.description} key={image.id} src={image.urls.regular}></img>
+            <ImageCard key={image.id} image={image} />
         )
     });
     return <div className="image-list"> {images}</div>
